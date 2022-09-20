615399b5deb8fa83511beb27a909136a43962f9d
febidamanik <febidamanik@gmail.com>
2022-09-20 17:23:29 +0700

    revised tugas3-pbp

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from mywatchlist.views import show_mywatchlist, show_mywatchlist_json, show_mywatchlist_xml
 
+app_name = 'mywatchlist'
+
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
     path('html/', show_mywatchlist, name='show_mywatchlist'),
